9819c3ff0a110a10844ae66fdaff4aa9f3023e5d
Leonardo Amaral <leoamaral.oliv@gmail.com>
2024-11-27 20:47:41 -0300

    save changes

--- cinemabiggu/tests.py
+++ cinemabiggu/tests.py
@@ -3,6 +3,8 @@ from rest_framework.test import APIRequestFactory
 from rest_framework.test import force_authenticate
 from django.contrib.auth import get_user_model
 from cinemabiggu.views import MovieViewSet
+from datetime import date
+import json
 
 
 class CinuemabigguTestCase(TestCase):
@@ -13,8 +15,30 @@ class CinuemabigguTestCase(TestCase):
         )
 
     def test_list_movies(self):
+        view = MovieViewSet.as_view({'get': 'list'})
         request = self.factory.get('/api/v1/movies')
         force_authenticate(request, user=self.user)
+        response = view(request)
         self.assertEqual(
-            MovieViewSet.as_view({'get': 'list'})(request).status_code, 200
+            response.status_code, 200
+        )
+
+    def test_insert_movies(self):
+        view = MovieViewSet.as_view({'post': 'dict'})
+        request = self.factory.post(
+            '/api/v1/movies/',
+            json.dumps({
+                "title": "test",
+                "description": "test",
+                "genre": "test",
+                "duration": 1,
+                "release_date": str(date.today()),
+                "avaliable": True
+            }),
+            content_type='application/json'
+        )
+        force_authenticate(request, user=self.user)
+        response = view(request)
+        self.assertEqual(
+            response.status_code, 201
         )
