bf5677d7d8dd797cea5605bff8367ab2566d84d9
Leonardo Amaral <leoamaral.oliv@gmail.com>
2024-12-04 22:49:16 -0300

    New views

--- cinemabiggu/views/__init__.py
+++ cinemabiggu/views/__init__.py
@@ -2,3 +2,5 @@ from .actors import ActorViewSet
 from .movies import MovieViewSet
 from .characters import CharacterViewSet
 from .genres import GenreViewSet
+from .rooms import RoomViewSet
+from .sessions import SessionViewSet
